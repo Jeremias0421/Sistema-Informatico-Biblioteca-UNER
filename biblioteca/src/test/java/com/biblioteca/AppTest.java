7d7ac95b68942fead4cde820829e8df0d4cbf32e
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-08-13 19:30:39 -0300

    Estructura inicial basica de Maven

---
+++ biblioteca/src/test/java/com/biblioteca/AppTest.java
@@ -0,0 +1,10 @@
+package com.biblioteca;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+   
+}
