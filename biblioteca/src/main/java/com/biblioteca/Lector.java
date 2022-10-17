afe90e7d2c20a4591ab87d9d1a4781beaea203b6
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-17 10:37:34 -0300

    Se agrega controles en el constructor

--- biblioteca/src/main/java/com/biblioteca/Lector.java
+++ biblioteca/src/main/java/com/biblioteca/Lector.java
@@ -18,13 +18,33 @@ public abstract class Lector extends Persona {
     public Lector(String nombre, String apellido, String sexo, LocalDate fechaNacimiento, String dni, TipoDni tipoDni,
             String correoElectronico, String nroCelular, String nacionalidad, String domicilio, int nroPostal,
             String departamento, String localidad) {
-                
+
         super(nombre, apellido, sexo, fechaNacimiento, dni, tipoDni);
-        setCorreoElectronico(correoElectronico);
+
+        if (!validarCorrero(correoElectronico))
+            throw new IllegalArgumentException("Correo Invalido");
+
+        if (!validarNroCelular(nroCelular))
+            throw new IllegalArgumentException("Nro de celular invalido");
+
+        if (!validarNacionalidad(nacionalidad))
+            throw new IllegalArgumentException("Nacionalidad invalida");
+
+        if (!validarDepartamento(departamento))
+            throw new IllegalArgumentException("Departamento invalido");
+
+        if (!validarLocalidad(localidad))
+            throw new IllegalArgumentException("Localidad invalida");
+
+        if (nroPostal < 0)
+            throw new IllegalArgumentException("Nro postal invalido");
+
+        this.correoElectronico = correoElectronico;
         this.nroCelular = nroCelular;
         this.nacionalidad = nacionalidad;
         this.domicilio = domicilio;
-        setNroPostal(nroPostal);
+        this.nroCelular = nroCelular;
+        this.nroPostal = nroPostal;
         this.departamento = departamento;
         this.localidad = localidad;
         this.multasAcumuladas = new ArrayList<>();
@@ -35,9 +55,9 @@ public abstract class Lector extends Persona {
     }
 
     public void devolverPrestamo() {
-        this.prestamo = null;//Idea: retornar el prestamo en otra variable para poder manipularlo fuera
+        this.prestamo = null;// Idea: retornar el prestamo en otra variable para poder manipularlo fuera
     }
-    
+
     public Prestamo getPrestamo() {
         return prestamo;
     }
@@ -51,7 +71,7 @@ public abstract class Lector extends Persona {
     }
 
     public void levantarMulta() {
-        //Completar
+        // Completar
     }
 
     public boolean isMultado() {
@@ -63,19 +83,9 @@ public abstract class Lector extends Persona {
     }
 
     public void setCorreoElectronico(String correoElectronico) {
-        String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-
-        if (correoElectronico.matches(regexPattern))
+        if (validarCorrero(correoElectronico)) {
             this.correoElectronico = correoElectronico;
-        else
-            throw new Error("Correo invalido");
-
-        // if (evaluarRegex(correoElectronico, regexPattern))
-        // this.correoElectronico = correoElectronico;
-        // else
-        // throw new Error("Correo invalido");
-
+        }
     }
 
     public String getNroCelular() {
@@ -83,7 +93,11 @@ public abstract class Lector extends Persona {
     }
 
     public void setNroCelular(String nroCelular) {
-        this.nroCelular = nroCelular;
+        if (validarNroCelular(nroCelular))
+            this.nroCelular = nroCelular;
+        else
+            throw new IllegalArgumentException("Nro de celular invalido");
+
     }
 
     public String getNacionalidad() {
@@ -91,7 +105,10 @@ public abstract class Lector extends Persona {
     }
 
     public void setNacionalidad(String nacionalidad) {
-        this.nacionalidad = nacionalidad;
+        if (validarNacionalidad(nacionalidad))
+            this.nacionalidad = nacionalidad;
+        else
+            throw new IllegalArgumentException("Nacionalidad invalida");
     }
 
     public String getDomicilio() {
@@ -107,11 +124,10 @@ public abstract class Lector extends Persona {
     }
 
     public void setNroPostal(int nroPostal) {
-        if (nroPostal > 0) {
+        if (nroPostal > 0)
             this.nroPostal = nroPostal;
-        } else {
-
-        }
+        else
+            throw new IllegalArgumentException("Nro postal invalido");
     }
 
     public String getDepartamento() {
@@ -119,7 +135,11 @@ public abstract class Lector extends Persona {
     }
 
     public void setDepartamento(String departamento) {
-        this.departamento = departamento;
+        if (validarDepartamento(departamento))
+            this.departamento = departamento;
+        else
+            throw new IllegalArgumentException("Departamento invalido");
+
     }
 
     public String getLocalidad() {
@@ -130,4 +150,26 @@ public abstract class Lector extends Persona {
         this.localidad = localidad;
     }
 
+    private boolean validarCorrero(String correo) {
+        String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
+        return correo.matches(regexPattern);
+    }
+
+    private boolean validarNroCelular(String celular) {
+        return celular.matches("^\\d{10}$");
+    }
+
+    private boolean validarNacionalidad(String nacionalidad) {
+        return nacionalidad.matches("[A-Z][a-zA-Z]*");
+    }
+
+    private boolean validarDepartamento(String departamento) {
+        return departamento.matches("([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)");
+    }
+
+    private boolean validarLocalidad(String localidad) {
+        return localidad.matches("([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)");
+    }
 }
