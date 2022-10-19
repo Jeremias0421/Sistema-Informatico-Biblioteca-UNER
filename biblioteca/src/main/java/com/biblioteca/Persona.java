72ea9a4fd32b8a4fd591c846265d6d47b2994d8b
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-19 10:30:12 -0300

    Se agrega metodos de control en el constructor y la documentacion de la clase

--- biblioteca/src/main/java/com/biblioteca/Persona.java
+++ biblioteca/src/main/java/com/biblioteca/Persona.java
@@ -3,8 +3,8 @@ package com.biblioteca;
 import java.time.LocalDate;
 
 /**
- * Esta clase {@code} Persona} representa una abstraccion de una persona
- * de la vida real.
+ * Persona es la clase base en la jerarquia de lectores de la biblioteca. Esta
+ * clase representa una abstraccion de una persona de la vida real.
  * 
  * @author Ezequiel Dalzotto
  * @author Jeremias Panozzo
@@ -19,75 +19,144 @@ public abstract class Persona {
     private TipoDni tipoDni;
 
     /**
-     * @param nombre
-     * @param apellido
-     * @param sexo
-     * @param fechaNacimiento
-     * @param dni
-     * @param tipoDni
-     * @throws IllegalArgumentException
+     * @param nombre          el nombre de la persona
+     * @param apellido        el apellido de la persona
+     * @param sexo            el sexo de la persona
+     * @param fechaNacimiento la fecha de nacimiento de la persona
+     * @param dni             el numero de documento de la persona
+     * @param tipoDni         el tipo de documento de la persona
+     * @throws IllegalArgumentException si el constructor recibe como parametros
+     *                                  valores invalidos para sus atributos
      */
     public Persona(String nombre, String apellido, String sexo, LocalDate fechaNacimiento, String dni,
             TipoDni tipoDni) {
 
-        if (nombre.matches("[A-Z][a-zA-Z]*")) {
-            this.nombre = nombre;
-        } else {
+        if (!validarNombre(nombre)) {
             throw new IllegalArgumentException("Nombre invalido.");
         }
 
-        if (apellido.matches("[a-zA-z]+([ '-][a-zA-Z]+)*")) {
-            this.apellido = apellido;
-        } else {
+        if (!validarApellido(apellido)) {
             throw new IllegalArgumentException("Apellido invalido.");
         }
 
-        if (sexo.equals("Masculino") || sexo.equals("Femenino")) {
-            this.sexo = sexo;
-        } else {
+        if (!validarSexo(sexo)) {
             throw new IllegalArgumentException("Sexo invalido.");
         }
 
-        if (fechaNacimiento.isAfter(LocalDate.of(1900, 1, 1)) && fechaNacimiento.isBefore(LocalDate.now())) {
-            this.fechaNacimiento = fechaNacimiento;
-        } else {
+        if (!validarFechaNacimiento(fechaNacimiento)) {
             throw new IllegalArgumentException("Fecha invalida");
         }
 
-        if (dni.matches("\\d{8}")) {
-            this.dni = dni;
-        } else {
+        if (!validarDni(dni)) {
             throw new IllegalArgumentException("DNI invalido.");
         }
-
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sexo = sexo;
+        this.fechaNacimiento = fechaNacimiento;
+        this.dni = dni;
         this.tipoDni = tipoDni;
     }
 
+    /**
+     * @return el nombre de la persona
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * 
+     * @return el apellido de la persona
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * 
+     * @return el sexo de la persona
+     */
     public String getSexo() {
         return sexo;
     }
 
+    /**
+     * @return la fecha de nacimiento de la persona
+     */
     public LocalDate getFechaNacimiento() {
         return fechaNacimiento;
     }
 
+    /**
+     * @return el numero de documento de la persona
+     */
     public String getDni() {
         return dni;
     }
 
+    /**
+     * @return el tipo de documento de la persona
+     */
     public TipoDni getTipoDni() {
         return tipoDni;
     }
 
+    /**
+     * @param tipoDni
+     */
     public void setTipoDni(TipoDni tipoDni) {
         this.tipoDni = tipoDni;
-    }    
+    }
+
+    /**
+     * 
+     * @param dni
+     * @return {@code true} si el numero de documento esta formado por ocho
+     *         caracteres numericos
+     */
+    private boolean validarDni(String dni) {
+        return dni.matches("\\d{8}");
+    }
+
+    /**
+     * 
+     * @param fechaNacimiento
+     * @return {@code true} si la edad es mayor a dieciseis años; de
+     *         cualquier otra forma {@code false}
+     */
+    private boolean validarFechaNacimiento(LocalDate fechaNacimiento) {
+        return fechaNacimiento.isAfter(LocalDate.of(1900, 1, 1))
+                && fechaNacimiento.isBefore(LocalDate.of(2006, 10, 22));
+    }
+
+    /**
+     * 
+     * @param sexo
+     * @return {@code true} si el sexo es Femenino o Masculino; de
+     *         cualquier otra forma {@code false}
+     */
+    private boolean validarSexo(String sexo) {
+        return sexo.equals("Masculino") || sexo.equals("Femenino");
+    }
+
+    /**
+     * 
+     * @param apellido
+     * @return {@code true} si es un appellido valido en el idioma castellano; de
+     *         cualquier otra forma {@code false}
+     */
+    private boolean validarApellido(String apellido) {
+        return apellido.matches("[a-zA-z]+([ '-][a-zA-Z]+)*");
+    }
+
+    /**
+     * 
+     * @param nombre
+     * @return {@code true} si es un nombre valido en el idioma castellano; de
+     *         cualquier otra forma {@code false}
+     */
+    private boolean validarNombre(String nombre) {
+        return nombre.matches("[A-Z][a-zA-Z]*");
+    }
 }
