72553bdb985eb3404475212a3e33f86856dcda8d
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-20 09:49:48 -0300

    Se agregan test de cambios de ubicacion

--- biblioteca/src/test/java/com/biblioteca/IdentificacionTest.java
+++ biblioteca/src/test/java/com/biblioteca/IdentificacionTest.java
@@ -1,27 +1,35 @@
 package com.biblioteca;
 
 import static org.junit.Assert.assertEquals;
-
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
 
 public class IdentificacionTest {
-    Identificacion identificacion = null;
-
-    @Before
-    public void setup(){
-        identificacion = new Identificacion(null, 5, 12, 8, 6, 1);
-    }
+    
     @Test
-    public void test1(){
-        assertEquals("5-12-8-6-1", identificacion.toString());
-        identificacion.cambiarUbicacion(2, 2, 7, 5, 10);
-        assertEquals("2-2-7-5-10", identificacion.toString());
-}
-
-    @Test
-    public void asignacionInvalidaTest(){
-        identificacion.cambiarUbicacion(-1, 2, 1, 1, 1);
+    public void test1() {
+        Identificacion identificacion = new Identificacion(null, 5, 12, 8, 6, 1);
         assertEquals("5-12-8-6-1", identificacion.toString());
     }
+
+    @Test
+    public void test2() {
+        Identificacion identificacion2 = new Identificacion(null, 5, 6, 7, 0, 7);
+        assertEquals("5-6-7-0-7", identificacion2.toString());
+    }
+
+    @Test
+    public void test3() {
+        Identificacion identificacion3 = new Identificacion(null, 10, 12, 7, 19, 7);
+        assertEquals("10-12-7-19-7", identificacion3.toString());
+    }
+
+    @Test
+    public void cambiarUbicacion(){
+        Identificacion identificacion3 = new Identificacion(null, 10, 12, 7, 19, 7);
+        assertEquals("10-12-7-19-7", identificacion3.toString());
+        identificacion3.cambiarUbicacion(1, 1, 1, 1, 1);
+        assertEquals("1-1-1-1-1", identificacion3.toString());
+    }
+
 }
