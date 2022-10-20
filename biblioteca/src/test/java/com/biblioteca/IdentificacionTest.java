eec4ab989a4928f826f3e4b489c22f195f4da566
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-20 09:59:55 -0300

    Se agregan mas tests

--- biblioteca/src/test/java/com/biblioteca/IdentificacionTest.java
+++ biblioteca/src/test/java/com/biblioteca/IdentificacionTest.java
@@ -2,10 +2,9 @@ package com.biblioteca;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
 
 public class IdentificacionTest {
-    
+
     @Test
     public void test1() {
         Identificacion identificacion = new Identificacion(null, 5, 12, 8, 6, 1);
@@ -25,11 +24,41 @@ public class IdentificacionTest {
     }
 
     @Test
-    public void cambiarUbicacion(){
+    public void cambiarUbicacion() {
         Identificacion identificacion3 = new Identificacion(null, 10, 12, 7, 19, 7);
         assertEquals("10-12-7-19-7", identificacion3.toString());
         identificacion3.cambiarUbicacion(1, 1, 1, 1, 1);
         assertEquals("1-1-1-1-1", identificacion3.toString());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void pasilloInvalidoTest() {
+        Identificacion identificacion = new Identificacion(null, -10, 12, 7, 19, 7);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void estanteInvalidoTest() {
+        Identificacion identificacion = new Identificacion(null, 10, -12, 7, 19, 7);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void filaInvalidaTest() {
+        Identificacion identificacion = new Identificacion(null, 10, 12, -7, 19, 7);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void columnaInvalidaTest() {
+        Identificacion identificacion = new Identificacion(null, 10, 12, 7, -19, 7);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void posicionInvalidaTest() {
+        Identificacion identificacion = new Identificacion(null, 10, 12, 7, 19, -7);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void cambiarUbicacionInvalida(){
+        Identificacion identificacion = new Identificacion(null, 10, 12, 7, 19, -7);
+        identificacion.cambiarUbicacion(-1, -1, -1, -1, -1);
+    }
 }
