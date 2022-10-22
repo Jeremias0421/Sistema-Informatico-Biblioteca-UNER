3eae94f048cf5bb01af0bae381387c690872b2c0
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-22 14:00:09 -0300

    Se agrega documentacion

--- biblioteca/src/main/java/com/biblioteca/Reserva.java
+++ biblioteca/src/main/java/com/biblioteca/Reserva.java
@@ -3,11 +3,31 @@ package com.biblioteca;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Esta clase representa una Reserva de ejemplares realizada por un Lector en un
+ * momento determinado.
+ * 
+ * @author Ezequiel Dalzotto
+ * @author Jeremias Panozzo
+ * @version 1.0
+ */
 public class Reserva {
+    /** La fecha en la que se hizo una Reserva */
     private LocalDate fecha;
+    /** Lector quien realizo la Reserva */
     private Lector lector;
+    /** Lista de ejemplares que se reservaron */
     private ArrayList<Ejemplar> ejemplares;
 
+    /**
+     * Instancia una nueva Reserva
+     * 
+     * @param fecha      fecha de la reserva
+     * @param lector     lector quien reserva
+     * @param ejemplares ejemplares reservados
+     * @exception IllegalArgumentException si la fecha de la reserva es anterior al
+     *                                     dia actual
+     */
     public Reserva(LocalDate fecha, Lector lector, ArrayList<Ejemplar> ejemplares) {
         if (fecha.isBefore(LocalDate.now())) {
             throw new IllegalArgumentException("Fecha anterior al dia actual");
@@ -17,14 +37,26 @@ public class Reserva {
         this.ejemplares = ejemplares;
     }
 
+    /**
+     * 
+     * @return Lector que hizo la reserva
+     */
     public Lector getLector() {
         return lector;
     }
 
+    /**
+     * 
+     * @return Lista de ejemplares que se reservaron
+     */
     public ArrayList<Ejemplar> getEjemplares() {
         return ejemplares;
     }
 
+    /**
+     * 
+     * @return fecha en la que se hizo la reserva
+     */
     public LocalDate getFecha() {
         return fecha;
     }
