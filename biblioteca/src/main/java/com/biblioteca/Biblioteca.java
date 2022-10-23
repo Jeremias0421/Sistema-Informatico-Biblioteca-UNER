da077c9232ce1452126cbe199ce2e6fc5f810e57
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-23 15:37:32 -0300

    Se agrega metodo ejemplaresDisponiblesSegunTematica

--- biblioteca/src/main/java/com/biblioteca/Biblioteca.java
+++ biblioteca/src/main/java/com/biblioteca/Biblioteca.java
@@ -4,107 +4,86 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * @author Ezequiel Dalzotto
+ * @author Jeremias Panozzo
+ * @version 1.0
+ */
 public class Biblioteca {
 
-    ArrayList<Ejemplar> ejemplaresDisponibles = null;
-    ArrayList<Ejemplar> ejemplaresPrestados = null;
-    ArrayList<Ejemplar> ejemplaresReservados = null;
+    ArrayList<Ejemplar> ejemplaresDisponibles = new ArrayList<>();
+    ArrayList<Ejemplar> ejemplaresPrestados = new ArrayList<>();
+    ArrayList<Ejemplar> ejemplaresReservados = new ArrayList<>();
+    ArrayList<Obra> listadoDeObras = new ArrayList<Obra>();
+    ArrayList<Lector> clientes = new ArrayList<>();
 
-    public static void main(String[] args) {
-        
+    public ArrayList<Lector> lectoresConObrasNoDevueltas() {
+        ArrayList<Lector> listado = new ArrayList<>();
+
+        for (Lector lector : clientes) {
+            if (lector.getPrestamo() != null) {
+                lector.getPrestamo().getPlazo();
+
+                listado.add(lector);
+            }
+        }
+        return listado;
     }
 
-    /** */
     public static void obrasMasSolicitadasAlumnosYDocentes() {
 
     }
 
-    /** */
     public static void obrasMasSolicitadasGeneral() {
 
     }
 
-    /** */
-    public static void ejemplaresDisponibles() {
+    public ArrayList<Ejemplar> ejemplaresDisponiblesSegunTematica(String areaReferencia) {
+        ArrayList<Ejemplar> lista = new ArrayList<>();
+        for (Obra obra : listadoDeObras) {
 
+            if (obra.getAreaTematica().equals(areaReferencia)) {
+
+                for (Ejemplar ejemplar : obra.getEjemplares()) {
+                    if (ejemplar.isDisponible()) {
+                        lista.add(ejemplar);
+                    }
+                }
+            }
+        }
+
+        return lista;
     }
 
-    /** */
     public static void obrasReservadas() {
 
     }
 
-    /** */
     public static void listarMultasPorPeriodo() {
 
     }
 
-    /**
-     * 
-     * @param editorial
-     */
     public static void listarPorEditorial(String editorial) {
 
     }
 
-    public static void darPrestamoDomicilio(Lector lector, ArrayList<Ejemplar> ejemplares, Funcionario funcionario, 
-        int plazo, ArrayList<Ejemplar> prestados, ArrayList<Ejemplar> disponibles, ArrayList<Ejemplar> reservados) {
+    public static void darPrestamoDomicilio(Lector lector, ArrayList<Ejemplar> ejemplares, Funcionario funcionario,
+            int plazo, ArrayList<Ejemplar> prestados, ArrayList<Ejemplar> disponibles, ArrayList<Ejemplar> reservados) {
 
-            for (Ejemplar ejemplar : ejemplares) {
-                if (ejemplar.getReserva() != null) {
-                    if (ejemplar.getReserva() != lector.getReserva()) { //Comprueba si fue reservado por el lector
-                        //Si Paso la fecha de la reserva saca la reserva
-                        if ((ejemplar.getReserva().getFecha()).isBefore(LocalDate.now())) {
-                            reservaVencida(ejemplar, reservados, disponibles);
-                        }else{
-                            throw new IllegalArgumentException(ejemplar + " Reservado");
-                        }
-                    }else{
-                        //Comprueba si fue reservado para el dia del prestamo
-                        if ((ejemplar.getReserva().getFecha()).isBefore(LocalDate.now())) {
-                            throw new IllegalArgumentException("Fecha de prestamo anticipada a la reservada");
-                        }else{
-                            levantarReserva(lector, reservados, disponibles);
-                        }
-                    }
-                }
-                if (!ejemplar.isDisponible()) {
-                    throw new IllegalArgumentException(ejemplar + ": No disponible");
-                }
-            }
-
-        if (lector.isMultado()) {
-            throw new IllegalArgumentException("Lector multado"); 
-        }
-        Prestamo prestamo = new Prestamo(plazo, Lectura.DOMICILO,
-        LocalDate.of(2022,10,15), funcionario, lector, ejemplares);
-        funcionario.tomarPrestamo(prestamo);
-        lector.pedirPrestamo(prestamo);
-        for (Ejemplar ejemplar : ejemplares) {
-            ejemplar.darEnPrestamo(prestamo);
-            prestados.add(ejemplar);
-            disponibles.remove(ejemplar);
-        }
-    }
-
-    public static void darPrestamoSala(Lector lector, ArrayList<Ejemplar> ejemplares, Funcionario funcionario, 
-        ArrayList<Ejemplar> prestados, ArrayList<Ejemplar> disponibles, ArrayList<Ejemplar> reservados) {
-
-        //Itera cada ejemplar para comprobar si esta reservado
         for (Ejemplar ejemplar : ejemplares) {
             if (ejemplar.getReserva() != null) {
-                if (ejemplar.getReserva() != lector.getReserva()) { //Comprueba si fue reservado por el lector
-                    //Si Paso la fecha de la reserva saca la reserva
-                    if ((ejemplar.getReserva().getFecha()).compareTo(LocalDate.now()) > 0) {
+                if (ejemplar.getReserva() != lector.getReserva()) { // Comprueba si fue reservado por el lector
+                    // Si Paso la fecha de la reserva saca la reserva
+                    if ((ejemplar.getReserva().getFecha()).isBefore(LocalDate.now())) {
                         reservaVencida(ejemplar, reservados, disponibles);
-                    }else{
+                    } else {
                         throw new IllegalArgumentException(ejemplar + " Reservado");
                     }
-                }else{
-                    //Comprueba si fue reservado para el dia del prestamo
-                    if ((ejemplar.getReserva().getFecha()).compareTo(LocalDate.now()) < 0) {
+                } else {
+                    // Comprueba si fue reservado para el dia del prestamo
+                    if ((ejemplar.getReserva().getFecha()).isBefore(LocalDate.now())) {
                         throw new IllegalArgumentException("Fecha de prestamo anticipada a la reservada");
-                    }else{
+                    } else {
                         levantarReserva(lector, reservados, disponibles);
                     }
                 }
@@ -113,13 +92,12 @@ public class Biblioteca {
                 throw new IllegalArgumentException(ejemplar + ": No disponible");
             }
         }
-        
+
         if (lector.isMultado()) {
             throw new IllegalArgumentException("Lector multado");
         }
-
-        Prestamo prestamo = new Prestamo(0, Lectura.SALA,
-        LocalDate.now(), funcionario, lector, ejemplares);
+        Prestamo prestamo = new Prestamo(plazo, Lectura.DOMICILO,
+                LocalDate.of(2022, 10, 15), funcionario, lector, ejemplares);
         funcionario.tomarPrestamo(prestamo);
         lector.pedirPrestamo(prestamo);
         for (Ejemplar ejemplar : ejemplares) {
@@ -129,8 +107,50 @@ public class Biblioteca {
         }
     }
 
-    public static void devolverPrestamo(Prestamo prestamo, Funcionario funcionario,ArrayList<Ejemplar>prestados,
-        ArrayList<Ejemplar> disponibles) {
+    public static void darPrestamoSala(Lector lector, ArrayList<Ejemplar> ejemplares, Funcionario funcionario,
+            ArrayList<Ejemplar> prestados, ArrayList<Ejemplar> disponibles, ArrayList<Ejemplar> reservados) {
+
+        // Itera cada ejemplar para comprobar si esta reservado
+        for (Ejemplar ejemplar : ejemplares) {
+            if (ejemplar.getReserva() != null) {
+                if (ejemplar.getReserva() != lector.getReserva()) { // Comprueba si fue reservado por el lector
+                    // Si Paso la fecha de la reserva saca la reserva
+                    if ((ejemplar.getReserva().getFecha()).compareTo(LocalDate.now()) > 0) {
+                        reservaVencida(ejemplar, reservados, disponibles);
+                    } else {
+                        throw new IllegalArgumentException(ejemplar + " Reservado");
+                    }
+                } else {
+                    // Comprueba si fue reservado para el dia del prestamo
+                    if ((ejemplar.getReserva().getFecha()).compareTo(LocalDate.now()) < 0) {
+                        throw new IllegalArgumentException("Fecha de prestamo anticipada a la reservada");
+                    } else {
+                        levantarReserva(lector, reservados, disponibles);
+                    }
+                }
+            }
+            if (!ejemplar.isDisponible()) {
+                throw new IllegalArgumentException(ejemplar + ": No disponible");
+            }
+        }
+
+        if (lector.isMultado()) {
+            throw new IllegalArgumentException("Lector multado");
+        }
+
+        Prestamo prestamo = new Prestamo(0, Lectura.SALA,
+                LocalDate.now(), funcionario, lector, ejemplares);
+        funcionario.tomarPrestamo(prestamo);
+        lector.pedirPrestamo(prestamo);
+        for (Ejemplar ejemplar : ejemplares) {
+            ejemplar.darEnPrestamo(prestamo);
+            prestados.add(ejemplar);
+            disponibles.remove(ejemplar);
+        }
+    }
+
+    public static void devolverPrestamo(Prestamo prestamo, Funcionario funcionario, ArrayList<Ejemplar> prestados,
+            ArrayList<Ejemplar> disponibles) {
         for (Ejemplar ejemplar : prestamo.getEjemplaresPrestados()) {
             prestados.remove(ejemplar);
             disponibles.add(ejemplar);
@@ -138,18 +158,20 @@ public class Biblioteca {
         }
         Lector lector = prestamo.getLector();
         funcionario.tomarDevolucion(prestamo);
-        if (Duration.between(prestamo.getFecha().atStartOfDay(), LocalDate.now().atStartOfDay()).toDays()>prestamo.getPlazo()) {
+        if (Duration.between(prestamo.getFecha().atStartOfDay(), LocalDate.now().atStartOfDay()).toDays() > prestamo
+                .getPlazo()) {
             lector.cargarMulta(new Multa(100f, 5, prestamo, lector));
         }
         lector.devolverPrestamo();
     }
 
-    public static void reservarEjemplares(Lector lector, LocalDate fecha, ArrayList<Ejemplar> ejemplares, ArrayList<Ejemplar> disponibles,
-        ArrayList<Ejemplar> reservados) {
-        
+    public static void reservarEjemplares(Lector lector, LocalDate fecha, ArrayList<Ejemplar> ejemplares,
+            ArrayList<Ejemplar> disponibles,
+            ArrayList<Ejemplar> reservados) {
+
         for (Ejemplar ejemplar : ejemplares) {
             if (!ejemplar.isDisponible()) {
-                if (ejemplar.getFechaBaja() != null || ejemplar.getReserva() !=null) {
+                if (ejemplar.getFechaBaja() != null || ejemplar.getReserva() != null) {
                     throw new IllegalArgumentException(ejemplar + "No disponible");
                 }
             }
@@ -176,8 +198,10 @@ public class Biblioteca {
         lector.reservarEjemplares(null);
     }
 
-    public static void reservaVencida(Ejemplar ejemplar, ArrayList<Ejemplar> reservados, ArrayList<Ejemplar> disponibles) {
-        //Revoca la reserva de todos los ejemplares, Metodo llamado cuando supero la fecha de reserva
+    public static void reservaVencida(Ejemplar ejemplar, ArrayList<Ejemplar> reservados,
+            ArrayList<Ejemplar> disponibles) {
+        // Revoca la reserva de todos los ejemplares, Metodo llamado cuando supero la
+        // fecha de reserva
         if ((ejemplar.getReserva().getFecha()).compareTo(LocalDate.now()) <= 0) {
             throw new IllegalArgumentException("Fecha de prestamo debe ser menor");
         }
