936d011e55c44818e935e4afd6ccccf2ccc611d6
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-24 02:09:51 -0300

    Se agrega documentacion

--- biblioteca/src/main/java/com/biblioteca/TipoFuncionario.java
+++ biblioteca/src/main/java/com/biblioteca/TipoFuncionario.java
@@ -1,5 +1,20 @@
 package com.biblioteca;
 
+/**
+ * Tipos de funcionarios registrados en el sistema.
+ * 
+ * @author Ezequiel Dalzotto
+ * @author Jeremias Panozzo
+ * @version 1.0
+ */
 public enum TipoFuncionario {
-    ADMIN,REGULAR
+
+    /**
+     * Representa a un Funcionario con permisos especiales para dar de alta y baja
+     * ejemplares.
+     */
+    ADMIN,
+
+    /** Funcionario comun sin permisos especiales. */
+    REGULAR
 }
