d19e469289ccfd2818b3f09a9ef0f053697c22dd
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-24 01:12:24 -0300

    Se agrega documentacion y nuevos tipos

--- biblioteca/src/main/java/com/biblioteca/TipoObra.java
+++ biblioteca/src/main/java/com/biblioteca/TipoObra.java
@@ -1,5 +1,14 @@
 package com.biblioteca;
 
+/**
+ * Tipos de Obras que se pueden encontrar en la Bilioteca.
+ * 
+ * @author Ezequiel Dalzotto
+ * @author Jeremias Panozzo
+ * @version 1.0
+ */
 public enum TipoObra {
-    LIBRO,REVISTA,ENSAYO,TESIS,MANUAL;
+    LIBRO, REVISTA, ENSAYO, TESIS, MANUAL, BOLETIN, COLECCION, COMPENDIO, DIARIO,
+    DICCIONARIO, DOCUMENTAL, ENCICLOPEDIA, INVESTIGACION,
+    MONOGRAFIA, PUBLICACIONES_MENSUALES;
 }
