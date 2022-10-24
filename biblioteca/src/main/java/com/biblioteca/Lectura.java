e09e244b89583238bd56b68aa195dc55e09d5076
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-24 02:06:15 -0300

    Se agrega documentacion

--- biblioteca/src/main/java/com/biblioteca/Lectura.java
+++ biblioteca/src/main/java/com/biblioteca/Lectura.java
@@ -1,5 +1,14 @@
 package com.biblioteca;
 
+/**
+ * Tipos de lecturas que se pueden realizar.
+ * 
+ * @author Ezequiel Dalzotto
+ * @author Jeremias Panozzo
+ * @version 1.0
+ */
 public enum Lectura {
-    SALA,DOMICILO
+
+    SALA,
+    DOMICILIO
 }
