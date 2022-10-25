d94b1c18e66386fcf63f7534e100b024c5d1e7a1
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2022-10-25 17:41:35 -0300

    Se agrega documentacion

--- biblioteca/src/main/java/com/biblioteca/Formato.java
+++ biblioteca/src/main/java/com/biblioteca/Formato.java
@@ -1,5 +1,11 @@
 package com.biblioteca;
 
+/** Formato habilitado para los Ejemplares almacenados en la Biblioteca. 
+ * 
+ * @author Ezequiel Dalzotto
+ * @author Jeremias Panozzo
+ * @version 1.0
+ */
 public enum Formato {
     ELECTRONICO,
     CD,
